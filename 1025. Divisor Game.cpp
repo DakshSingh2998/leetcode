ecfbb9c8a30c3557f903208aea32862f4e79ecb5
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2022-04-29 00:12:43 +0530

    Create 1025. Divisor Game.cpp

---
+++ 1025. Divisor Game.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/divisor-game/
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+};
